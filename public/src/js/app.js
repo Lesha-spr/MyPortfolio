72a1f190e6cf9a8d5ce4fac1d654c36681a2d1ea
Oleksii An <Oleksii_An@epam.com>
2015-06-24 17:55:46 +0300

    RCRT-0019: prevent scrolling on transitions.

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -12,7 +12,7 @@ var ProjectDetails = require('./components/ProjectDetails.react');
 
 // Declare our routes and their hierarchy
 var routes = (
-    <Route handler={App}>
+    <Route ignoreScrollBehavior handler={App}>
         <Route name='home' path='/' handler={Home}/>
         <Route name='about' path='about' handler={About}/>
         <Route name='projects' path='projects' handler={Projects}/>
