e469ddfdf1ebf522e31a79d67c16e90f9a08f15b
Oleksii An <Oleksii_An@epam.com>
2015-06-25 12:38:26 +0300

    RCRT-0000: minor code style fix.

--- start.js
+++ start.js
@@ -6,6 +6,6 @@ var server = app.listen(config.port, function () {
     var host = server.address().address;
     var port = server.address().port;
 
-    console.log('Example app listening at http://%s:%s', host, port)
+    console.log('Example app listening at http://%s:%s', host, port);
 
 });
