8fbb5ece470392d83cadd80a795526f992767e03
Lesha-spr <voodoo.spr@gmail.com>
2015-07-05 19:21:19 +0300

    RCRT-0000: prepare deploy, port issue.

--- start.js
+++ start.js
@@ -1,7 +1,7 @@
 var app = require('./app');
 var config = require('./config/config.json');
 
-var server = app.listen(config.port, function () {
+var server = app.listen(process.env.PORT || config.port, function () {
 
     var host = server.address().address;
     var port = server.address().port;
