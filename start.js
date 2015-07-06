9f449a6b468b37a3eab68537099f4a304b9704d3
Oleksii An <Oleksii_An@epam.com>
2015-07-06 18:55:36 +0300

    RCRT-0000: minify images.

--- start.js
+++ start.js
@@ -6,6 +6,7 @@ var server = app.listen(process.env.PORT || config.port, function () {
     var host = server.address().address;
     var port = server.address().port;
 
+
     console.log('Example app listening at http://%s:%s', host, port);
 
 });
