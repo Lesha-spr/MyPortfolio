758369482bc7155db5fc1c8139b6ff843bd798c0
Oleksii An <Oleksii_An@epam.com>
2015-07-07 16:52:15 +0300

    RCRT-0000: merge.

--- start.js
+++ start.js
@@ -1,21 +1,11 @@
 var app = require('./app');
 var config = require('./config/config.json');
 
-var Imagemin = require('imagemin');
+var server = app.listen(process.env.PORT || config.port, function () {
 
-new Imagemin()
-    .src('./public/src/i/*.{gif,jpg,png,svg}')
-    .dest('./public/build/i')
-    .use(Imagemin.optipng({progressive: true}))
-    .use(Imagemin.jpegtran({progressive: true}))
-    .run(function (err, files) {
-        console.log(files);
-        var server = app.listen(process.env.PORT || config.port, function () {
+    var host = server.address().address;
+    var port = server.address().port;
 
-            var host = server.address().address;
-            var port = server.address().port;
+    console.log('Example app listening at http://%s:%s', host, port);
 
-            console.log('Example app listening at http://%s:%s', host, port);
-
-        });
 });
