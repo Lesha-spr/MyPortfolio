382e1a74b73ac25bb814db04fdb5711ca58b58b7
Oleksii An <Oleksii_An@epam.com>
2015-07-07 16:52:15 +0300

    RCRT-0000: merge.

--- start.js
+++ start.js
@@ -6,7 +6,5 @@ var server = app.listen(process.env.PORT || config.port, function () {
     var host = server.address().address;
     var port = server.address().port;
 
-
     console.log('Example app listening at http://%s:%s', host, port);
-
 });
