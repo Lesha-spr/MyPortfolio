9d2a97769aa2d36274cd5c18ee0d05cca0bda40a
Lesha-spr <voodoo.spr@gmail.com>
2015-07-21 00:41:14 +0300

    RCFV-0001: Started form validation component.

--- public/src/js/components/Home.react.js
+++ public/src/js/components/Home.react.js
@@ -1,10 +1,13 @@
 var React = require('react');
 var Technologies = require('./Technologies.react');
+var Login = require('./Login');
+
 
 var Home = React.createClass({
     render: function() {
         return (
             <div>
+                <Login/>
                 <h2>Homepage</h2>
                 <Technologies/>
             </div>
