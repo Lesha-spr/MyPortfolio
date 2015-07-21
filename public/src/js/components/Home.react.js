5fc9683f2e028364ad50d17cce91396cda9c8ca2
Oleksii An <Oleksii_An@epam.com>
2015-07-21 13:35:15 +0300

    RCRT-0000: moved Validation to separated module.

--- public/src/js/components/Home.react.js
+++ public/src/js/components/Home.react.js
@@ -1,13 +1,11 @@
 var React = require('react');
 var Technologies = require('./Technologies.react');
-var Login = require('./Login');
 
 
 var Home = React.createClass({
     render: function() {
         return (
             <div>
-                <Login/>
                 <h2>Homepage</h2>
                 <Technologies/>
             </div>
