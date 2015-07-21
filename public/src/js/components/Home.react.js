e686540580ac538398b5a21e65a6be7c87795f45
Lesha-spr <voodoo.spr@gmail.com>
2015-07-22 00:49:26 +0300

    RCFV-0002: working on validation component.

--- public/src/js/components/Home.react.js
+++ public/src/js/components/Home.react.js
@@ -1,11 +1,28 @@
 var React = require('react');
 var Technologies = require('./Technologies.react');
+var Validation = require('./Validation');
 
+var Login = React.createClass({
+    onSubmit: function(event) {
+        event.preventDefault();
+        console.log(event.currentTarget);
+    },
+
+    render: function() {
+        return (
+            <Validation.Form onSubmit={this.onSubmit}>
+                <Validation.Input className='ui-input' validations={['isRequired', 'isEmail']} name='username' type='text'/>
+                <Validation.Input className='ui-input' validations={['isRequired']} name='password' type='password'/>
+            </Validation.Form>
+        );
+    }
+});
 
 var Home = React.createClass({
     render: function() {
         return (
             <div>
+                <Login/>
                 <h2>Homepage</h2>
                 <Technologies/>
             </div>
--- public/src/js/components/Validation.js
+++ public/src/js/components/Validation.js
@@ -1,10 +1,36 @@
 var React = require('react');
 var _ = require('underscore');
+var validator = require('validator');
+var classNames = require('classnames');
+
+validator.extend('isRequired', function(str) {
+    return validator.trim(str).length;
+});
+
+var ERRORS = {
+    DEFAULT_MESSAGE: 'validation error',
+    isValid: {
+        className: 'ui-input_state_invalid'
+    },
+
+    isRequired: {
+        className: 'ui-input_state_empty',
+        message: 'required'
+    },
+
+    isEmail: {
+        className: 'ui-input_state_email-pattern-failed',
+        message: 'should be email'
+    }
+};
 
 var Validation = {
     Form: React.createClass({
-        render: function() {
+        componentWillMount: function() {
+            this.inputs = [];
+        },
 
+        render: function() {
             return (
                 <form onSubmit={this.props.onSubmit}>
                     {this.recursiveCloneChildren(this.props.children)}
@@ -12,8 +38,37 @@ var Validation = {
             );
         },
 
-        validate: function(event) {
-            console.log(event.currentTarget);
+        validate: function(component) {
+            var rules = component.props.validations;
+            var isValid = true;
+            var classes = {};
+            var errorMessage;
+
+            classes[component.props.className] = true;
+
+            for (var i = 0; i < rules.length; i++) {
+                var rule = rules[i];
+
+                if (!validator[rule](component.state.value)) {
+                    isValid = false;
+                    classes[ERRORS.isValid.className] = true;
+                    classes[ERRORS[rule].className] = true;
+                    errorMessage = ERRORS[rule].message || ERRORS.DEFAULT_MESSAGE;
+
+                    break;
+                }
+            }
+
+            var className = classNames(classes);
+
+            component.setState({
+                isValid: isValid,
+                isUsed: true,
+                className: className,
+                errorMessage: errorMessage || null
+            });
+
+            console.log(this.inputs);
         },
 
         recursiveCloneChildren: function(children) {
@@ -22,7 +77,18 @@ var Validation = {
                     return child;
                 }
 
-                var childProps = child.props.name ? {validate: this.validate} : {};
+                var childProps;
+                var shouldValidate = _.isArray(child.props.validations) && child.props.validations.length;
+
+                if (shouldValidate) {
+                    childProps = {
+                        validate: this.validate
+                    };
+
+                    this.inputs.push(child.props.name);
+                } else {
+                    childProps = {};
+                }
 
                 childProps.children = this.recursiveCloneChildren(child.props.children);
 
@@ -32,6 +98,34 @@ var Validation = {
     }),
 
     Input: React.createClass({
+        getInitialState: function() {
+            return {
+                value: this.props.value || '',
+                className: this.props.className || '',
+                isValid: true,
+                isUsed: false,
+                errorMessage: null
+            }
+        },
+
+        setValue: function(event) {
+            this.setState({
+                value: event.currentTarget.value
+            }, function() {
+                if (this.state.isUsed) {
+                    this.props.validate(this);
+                }
+            });
+
+            (this.props.onChange || _.noop)(event);
+        },
+
+        onBlur: function(event) {
+            this.props.validate(this);
+
+            (this.props.onBlur || _.noop)(event);
+        },
+
         getDefaultProps: function() {
             return {
                 type: 'text',
@@ -41,26 +135,13 @@ var Validation = {
 
         render: function() {
             return (
-                <input type={this.props.type} onChange={this.props.validate} name={this.props.name}/>
+                <div>
+                    <input {...this.props} className={this.state.className} value={this.state.value} onChange={this.setValue} onBlur={this.onBlur}/>
+                    <span className='ui-input-hint'>{this.state.errorMessage}</span>
+                </div>
             )
         }
     })
 };
-//
-//var Login = React.createClass({
-//    onSubmit: function(event) {
-//        event.preventDefault();
-//        console.log(event.currentTarget);
-//    },
-//
-//    render: function() {
-//        return (
-//            <Form onSubmit={this.onSubmit}>
-//                <Input name='username' type='text'/>
-//                <Input name='password' type='password'/>
-//            </Form>
-//        );
-//    }
-//});
 
 module.exports = Validation;
