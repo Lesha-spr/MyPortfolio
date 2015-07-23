c463ab663a2f0ca686425f2da7c55ebe3f36f13c
Oleksii An <Oleksii_An@epam.com>
2015-07-23 16:44:04 +0300

    RCRT-0000: removed validation to separated module.

--- public/src/js/components/Home.react.js
+++ public/src/js/components/Home.react.js
@@ -1,29 +1,10 @@
 var React = require('react');
 var Technologies = require('./Technologies.react');
-var Validation = require('./Validation');
-
-var Login = React.createClass({
-    onSubmit: function(event) {
-        event.preventDefault();
-        console.log(event.currentTarget);
-    },
-
-    render: function() {
-        return (
-            <Validation.Form onSubmit={this.onSubmit}>
-                <Validation.Input blocking='input' className='ui-input' validations={['isRequired', 'isEmail']} name='username' type='text'/>
-                <Validation.Input blocking='input' className='ui-input' validations={['isRequired']} name='password' type='password'/>
-                <Validation.Button blocking='button' value='submit'/>
-            </Validation.Form>
-        );
-    }
-});
 
 var Home = React.createClass({
     render: function() {
         return (
             <div>
-                <Login/>
                 <h2>Homepage</h2>
                 <Technologies/>
             </div>
