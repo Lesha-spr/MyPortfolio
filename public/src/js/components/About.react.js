4bdc09002f1090f9ceed6e2817f287a6ad500fcc
Oleksii An <Oleksii_An@epam.com>
2015-09-10 11:55:27 +0300

    RCRT-0055: rewrited 'about' section.

--- public/src/js/components/About.react.js
+++ public/src/js/components/About.react.js
@@ -1,4 +1,5 @@
 var React = require('react');
+var Link = require('react-router').Link;
 
 var About = React.createClass({
     render: function() {
@@ -7,12 +8,14 @@ var About = React.createClass({
                 <h2>About</h2>
                 <img className='about__pic' src='/src/i/me.jpg' alt='me'/>
                 <div className='about__story'>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sodales erat velit, ac commodo libero luctus at. Cras posuere luctus lorem in tristique. Vivamus elementum sit amet urna non rutrum. In aliquet est sit amet purus congue aliquam. Maecenas facilisis ultrices metus eget mollis. Ut at porta enim. Proin tempus ex in tortor aliquam, sit amet auctor felis varius. Aenean rhoncus, turpis eget euismod lobortis, justo ligula sodales sem, a posuere elit est id lectus. Aenean in velit eget augue efficitur blandit. Ut non ultricies sem, ut varius nunc. Donec rhoncus justo sit amet nisi gravida, non dignissim quam iaculis. Morbi suscipit hendrerit varius.</p>
-                    <p>Mauris vestibulum posuere nulla. Cras fermentum vitae neque in dignissim. Aliquam tristique consequat facilisis. Duis scelerisque laoreet sapien eu ullamcorper. Maecenas maximus turpis leo, id sagittis orci interdum semper. Integer non eros consectetur, semper ipsum eu, tempor ante. Aliquam fringilla eget augue sed semper. Nulla posuere ex at nibh mattis maximus. Quisque in convallis libero. Nunc enim felis, ornare in pharetra non, efficitur eu ipsum. Fusce est orci, condimentum quis sem vel, bibendum dignissim lacus. Sed tincidunt, justo ut gravida volutpat, mi lacus tempus nibh, ut gravida nulla enim in dui. Duis cursus arcu vitae ante lacinia, at ornare neque cursus. Mauris vitae arcu feugiat, pellentesque ligula id, malesuada ante. Maecenas vulputate malesuada placerat.</p>
-                    <p>Nullam a pharetra est. Fusce ac justo nec mi aliquam eleifend quis in erat. Nulla in eleifend velit. Pellentesque eu enim in ligula auctor placerat. Proin fermentum massa vitae lacinia varius. Nullam condimentum vehicula nisl, at maximus tortor malesuada sit amet. Mauris vel dictum augue.</p>
-                    <p>Suspendisse vestibulum faucibus purus vitae volutpat. In vel eros id ligula tristique bibendum quis id libero. Aliquam vitae nisi tellus. Fusce elit est, consequat id cursus sit amet, vehicula id nunc. Praesent et commodo augue. Maecenas risus turpis, vestibulum in vulputate eu, porttitor sed nisi. Sed consectetur rhoncus erat, eu rutrum dolor fringilla ac. Duis a velit sed ex fermentum faucibus ac eget augue. Nunc fermentum, nisl at molestie eleifend, metus felis commodo ipsum, euismod semper leo odio in urna. Pellentesque pretium pharetra justo, at maximus nibh bibendum quis. Donec vulputate nunc et ullamcorper pellentesque. Sed ultricies dictum nulla at volutpat. Integer sed justo eu ligula placerat fermentum.</p>
-                    <p>Morbi congue tincidunt dictum. Aenean ultricies nibh in massa ullamcorper, vehicula ultricies orci bibendum. Quisque malesuada magna eget neque fringilla congue. Etiam sed mattis enim. Cras ac dignissim ante. Sed ac enim lectus. Quisque ligula justo, tempus in ex nec, finibus viverra ex. Proin tempus, quam quis imperdiet volutpat, metus sem porta felis, vel ullamcorper lectus nulla id purus.</p>
-                    </div>
+                    <p>Hi there!</p>
+                    <p>My name is Oleksii An and I'm a Frontend developer.</p>
+                    <p>I love my job and everything about Frontend engineering. I got about 4 years experience in developing high loaded, high performance and user-friendly <Link to='projects'>applications</Link>. Also I'm contributing to open-source projects and have <a href='https://www.npmjs.com/~lesha-spr'>my own</a>.</p>
+                    <p>For me the most important things is <b>quality</b>, <b>expertise</b> and <b>passion</b>. In that order. I believe, as more <b>quality</b> you do, as more proficient you become and more challenging projects you can have. <b>Expertise</b> is the second one in my order, but not the less important. It's quite the same, but with an perceived scale. Well, <b>passion</b> is a sleepless nights and crazy days with coding and coding :)</p>
+                    <p>You can <Link to='contacts'>contact</Link> me anytime and anyhow with any questions. I'll be happy to answer :)</p>
+                    <p>Cheers and thanks for your time!</p>
+                    <p>Kind regards, <br/>Oleksii</p>
+                </div>
             </div>
         );
     }
